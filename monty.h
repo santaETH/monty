e08abff671f056754fb933ee8702f1b4d04bcad7
santaETH <sdamissie@yahoo.com>
2023-12-26 23:18:53 -0800

    C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -39,17 +39,29 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-char *_strchr_(const char *, int);
-char *_strstr_(const char *, const char *);
-int _readfile_(char **, int);
-int _readline_(char *);
-char *_command_(const char *);
-int _value_(const char *);
-int _exe_(const char *, stack_t **);
-
-stack_t *push(stack_t **, unsigned int);
-stack_t *pall(stack_t **, unsigned int);
-stack_t *free_struct(stack_t *);
-void fix_head(stack_t *);
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 
 #endif /*MONTY_H*/
